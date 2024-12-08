5e26f4f1d530a411adfd96ec2d3128d48c4984f7
erengunaydin <tur69064@temple.edu>
2024-12-08 00:51:42 -0500

    finally working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import musci
 
 if __name__ == "__main__":
-    musci.run_bot
+    musci.run_bot()
--- musci.py
+++ musci.py
@@ -9,13 +9,13 @@ def run_bot():
     TOKEN = os.getenv('discord_token')
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents)
+    client = discord.Client(intents=intents)
 
     voice_clients = {}
     yt_dl_options = {"format": "bestaudio/best"}
     ytdl = yt_dlp.YoutubeDL(yt_dl_options)
 
-    ffmpeg = {'options': 'vn'}
+    ffmpeg_options = {'options': 'vn'}
 
     @client.event
     async def on_ready():
@@ -23,21 +23,21 @@ def run_bot():
 
         @client.event
         async def on_message(message):
-            if message.contnet.startwith("?play"):
+            if message.content.startswith("?play"):
                 try:
                     voice_client = await message.author.voice.channel.connect()
-                    voice_clients=[voice_client.guild.id] = voice_client
+                    voice_clients[voice_client.guild.id] = voice_client
                 except Exception as e:
                     print(e)
 
                 try:
-                    url = message.cotnent.split()[1]
+                    url = message.content.split()[1]
                     
                     loop = asyncio.get_event_loop()
                     data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
 
                     song = data['url']
-                    player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
+                    player = discord.FFmpegOpusAudio(song, **ffmpeg_options)
 
                     voice_clients[message.guild.id].play(player)
                 except Exception as e:
